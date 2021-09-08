73852db8665983f0628d7f70d914ea8fff1ebffc
NARESH <nareshshet143@gmail.com>
2021-09-08 12:57:39 +0530

    Functions Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import React, { Component, useState } from "react";
 import NavBar from "./components/NavBar";
+import TextForm from "./components/TextForm";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <TextForm />
       </div>
     );
   }
